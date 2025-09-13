7b1dfbe3d821d7e669a015e8841d593190c998a2
Andrey Solovyov <solovyov.net@gmail.com>
2025-09-13 21:48:35 +0300

    add database.sql

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE urls (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL UNIQUE,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code integer,
+    h1 varchar(255),
+    title varchar(255),
+    description text,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+); 
